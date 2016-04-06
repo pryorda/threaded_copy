436cbb98508727bc808bb98fcc46505ee285faa9
Daniel Pryor <pryorda@users.noreply.github.com>
2016-04-06 12:50:05 -0600

    Fixing stuff

--- threaded_copy.py
+++ threaded_copy.py
@@ -6,7 +6,7 @@ import subprocess
 import time
 
 processes = set()
-max_processes = 20
+max_processes = 10
 
 # Usage
 parser = OptionParser()
@@ -29,16 +29,18 @@ src_dir = options.src_dir
 dest_dir = options.dest_dir
 
 for file in os.listdir(src_dir):
-   new_file = re.sub(r'^\d{5}', '', file)
+#   new_file = re.sub(r'^\d{5}', '', file)
 
-   old_file_src = src_dir + "\\" + file
-   new_file_dest = dest_dir + "\\" + new_file
+   old_file_src = src_dir + "/" + file
+   new_file_dest = dest_dir + "/" + file
+#   new_file_dest = dest_dir + "/" + newfile
 
-   command1 = "copy \"" + old_file_src + "\" \"" + new_file_dest + "\" >nul"
+   command1 = "cp -anrp \"" + old_file_src + "\" \"" + new_file_dest + "\""
 
    processes.add(subprocess.Popen(command1, shell=True))
-   if len(processes) >= max_processes:
-      time.sleep(.1)
+   print "Current # of Process", len(processes)
+   while len(processes) >= max_processes:
+      time.sleep(1)
       processes.difference_update([p for p in processes if p.poll() is not None])
 
 for p in processes:
